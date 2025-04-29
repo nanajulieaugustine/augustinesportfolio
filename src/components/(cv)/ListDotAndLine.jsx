f4f243434ce866b6f797bcaf40c4d29d3ed7f82d
nanajulieaugustine <naje0004@stud.kea.dk>
2025-04-29 19:41:09 +0200

    list update

--- src/components/(cv)/ListDotAndLine.jsx
+++ src/components/(cv)/ListDotAndLine.jsx
@@ -1,8 +1,13 @@
-const ListDotAndLine = () => {
+const ListDotAndLine = ({ onClick, className }) => {
   return (
-    <div className="relative flex justify-center items-center">
-      <div className="absolute left-1/2 transform -translate-x-1/2 w-0.5 h-42 bg-(--white-primary)"></div>
-      <div className="w-5 h-5 rounded-full border-2 border-(--white-primary) bg-(--white-primary) z-1"></div>
+    <div
+      className="relative flex justify-center items-center"
+      onClick={onClick}
+    >
+      <div className="absolute left-1/2 transform -translate-x-1/2 w-0.5 h-55 bg-(--white-primary) -z-1"></div>
+      <div
+        className={`w-5 h-5 rounded-full border-2 border-(--white-primary) bg-(--white-primary) z-1 cursor-pointer ${className}`}
+      ></div>
     </div>
   );
 };
